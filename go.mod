78adc687a03c02311f7cfa9faf68eeb21ad734ed
alexandrperry <alexch@wix.com>
2023-01-23 02:03:45 +0200

    feat: add grpc service with create and login methods

--- go.mod
+++ go.mod
@@ -9,14 +9,21 @@ require (
 	github.com/stretchr/testify v1.8.1
 )
 
-require github.com/golang-jwt/jwt/v4 v4.4.3
+require (
+	github.com/aead/chacha20poly1305 v0.0.0-20201124145622-1a5aba2a8b29
+	github.com/golang-jwt/jwt/v4 v4.4.3
+	github.com/o1egl/paseto v1.0.0
+	github.com/techschool/simplebank v0.0.0-20230102174032-2ecf697a5571
+	google.golang.org/grpc v1.50.1
+)
 
 require (
 	github.com/aead/chacha20 v0.0.0-20180709150244-8b13a72661da // indirect
-	github.com/aead/chacha20poly1305 v0.0.0-20170617001512-233f39982aeb // indirect
 	github.com/aead/poly1305 v0.0.0-20180717145839-3fee0db0b635 // indirect
-	github.com/o1egl/paseto v1.0.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.10.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	google.golang.org/genproto v0.0.0-20221024183307-1bc688fe9f3e // indirect
 )
 
 require (
@@ -50,7 +57,7 @@ require (
 	golang.org/x/net v0.5.0 // indirect
 	golang.org/x/sys v0.4.0 // indirect
 	golang.org/x/text v0.6.0 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
+	google.golang.org/protobuf v1.28.1
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- main.go
+++ main.go
@@ -3,17 +3,22 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net"
 
 	"github.com/alexandrperry/simplebank/api"
 	db "github.com/alexandrperry/simplebank/db/sqlc"
+	"github.com/alexandrperry/simplebank/gapi"
+	"github.com/alexandrperry/simplebank/pb"
 	"github.com/alexandrperry/simplebank/util"
 	_ "github.com/lib/pq"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
 	config, err := util.LoadConfig(".")
 	if err != nil {
-		log.Fatal("cannot load config", err)
+		log.Fatal("cannot load config:", err)
 	}
 
 	conn, err := sql.Open(config.DBDriver, config.DBSource)
@@ -22,12 +27,38 @@ func main() {
 	}
 
 	store := db.NewStore(conn)
+	runGrpcServer(config, store)
+}
+
+func runGrpcServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server:", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterSimpleBankServer(grpcServer, server)
+	reflection.Register(grpcServer)
+
+	listener, err := net.Listen("tcp", config.GRPCServerAddress)
+	if err != nil {
+		log.Fatal("cannot create listener")
+	}
+
+	log.Printf("start gRPC server at %s", listener.Addr().String())
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("cannot start gRPC server")
+	}
+}
+
+func runGinServer(config util.Config, store db.Store) {
 	server, err := api.NewServer(config, store)
 	if err != nil {
 		log.Fatal("cannot create server:", err)
 	}
 
-	err = server.Start(config.ServerAddress)
+	err = server.Start(config.HTTPServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
